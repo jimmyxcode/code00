91686d3c0599c4ed0ce246bca10a4a4357bd85d5
Jimmy <jimmyxcode@icloud.com>
2025-10-16 20:47:28 +0100

    Initial Commit

---
+++ LifeEveryday/LifeEverydayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeEverydayApp.swift
+//  LifeEveryday
+//
+//  Created by jimmyxcode on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct LifeEverydayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LifeEverydayTests/LifeEverydayTests.swift
@@ -0,0 +1,16 @@
+//
+//  LifeEverydayTests.swift
+//  LifeEverydayTests
+//
+//  Created by jimmyxcode on 16/10/2025.
+//
+
+import Testing
+
+struct LifeEverydayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
